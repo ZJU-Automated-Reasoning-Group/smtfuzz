a129b02e04d76f0c47b97cbcc1d2c8f3f230731e
rainoftime <rainoftime@gmail.com>
2025-06-09 14:52:03 +0800

    Add scripts for commit analysis and checking; remove unused storm documentation and assets

---
+++ scripts/changes/analyze_commits.py
@@ -0,0 +1,22 @@
+"""
+Analyze the fixing commits
+
+- The changed files, function, lines, etc.
+"""
+
+import argparse
+import os
+import sys
+import json
+import subprocess
+import time
+import shutil
+import re
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
